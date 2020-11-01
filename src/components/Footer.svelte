35477a2e1255700426f534f7361d088545ca4e12
Héctor Lemus <dev.hectorlemus@gmail.com>
2020-11-01 01:28:20 -0600

    projecto del curso de svelte

---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<div class="Footer">
+    <div class="Fotter-container">
+        <div class="Fotter-copy">
+            <span>2020 MATCH DAY FROM HEART :)</span>
+        </div>
+    </div>
+</div>
+
+<style>
+    .Footer {
+      font-size: 11px;
+      letter-spacing: 1px;
+      font-weight: normal;
+      color: #c7c7c7;
+    }
+</style>
